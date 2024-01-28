af9fcacb495e6fd30184433982043db562886803
FoxyBryn <155707565+FoxyBryn@users.noreply.github.com>
2024-01-28 11:44:04 +1100

    Add page layout elements and basic css.

---
+++ scripts.js
@@ -0,0 +1,13 @@
+/* Plan
+
+create grid of squares
+allow user input to adjust grid size
+
+on mouseover event change each square colour
+
+allow user to choose whether etch-a-sketch fills with black or randomised colours
+
+allow user to reset grid
+
+
+*/
