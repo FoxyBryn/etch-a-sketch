dbd47cb533720342638d1b7dca69041e60aed929
FoxyBryn <155707565+FoxyBryn@users.noreply.github.com>
2024-01-30 13:59:20 +1100

    Add js functionality to buttons & grid mouseover.

--- scripts.js
+++ scripts.js
@@ -15,6 +15,10 @@ allow user to reset grid
 const gridContainer = document.querySelector('.grid-container');
 const slider = document.querySelector('.slider');
 const userInputText = document.querySelector('.user-input-text');
+const btnColour = document.querySelector('.colour');
+const btnDark = document.querySelector('.dark');
+const btnReset = document.querySelector('.reset');
+
 
 function createGrid(size) {
     gridContainer.innerHTML = '';
@@ -28,20 +32,41 @@ function createGrid(size) {
             square.style.flexBasis = `calc(${ratio}% - 2px)`;
             square.classList.add('grid-square');
             gridContainer.appendChild(square);
+
+            square.addEventListener('mouseover', function() {
+                if (colourSelect === true) {
+                    square.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+                } else {
+                    square.style.backgroundColor = '#0a134d';
+                }
+            });
+            btnReset.addEventListener('click', function() {
+                square.style.backgroundColor = 'white';
+            });
+
         }
 
-        const newLine = document.createElement('div');
-        gridContainer.appendChild(newLine)
+        const nextRow = document.createElement('div');
+        gridContainer.appendChild(nextRow)
         
     }
-}
+};
 
 createGrid(slider.value);
-
-
 
 slider.addEventListener('input', function() {
     createGrid(this.value);
     userInputText.textContent = `${this.value}x${this.value}`;
 });
+
+let colourSelect;
+
+btnDark.addEventListener('click', function() {
+    colourSelect = false;
+});
+
+btnColour.addEventListener('click', function() {
+    colourSelect = true;
+});
+
 
